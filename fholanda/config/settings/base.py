e3e32b9e30e9bc1a6975e7985268b4d84d335718
Darpan Deva <flp9001@gmail.com>
2019-09-25 13:13:49 -0300

    BUGFIX: urls.py path on settings

--- fholanda/config/settings/base.py
+++ fholanda/config/settings/base.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'fholanda.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'fholanda.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
